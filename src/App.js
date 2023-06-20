e1e624a8aca5e1e508e811ac64a9c1daca70aa4a
J0n1i <joni.k.123@hotmail.fi>
2023-06-20 16:43:36 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
 function App() {
+  const [longUrl, setLongUrl] = useState("")
+  const [urls, setUrls] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/")
+      .then(res => res.json())
+      .then(data => setUrls(data))
+  }, [])
+
+  console.log(urls)
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("http://localhost:8080/shorturl", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ url: longUrl })
+    }).then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setUrls([...urls, data.shortUrl])
+      })
+  }
+
+  function handleClick(index) {
+    window.open("http://localhost:8080/" + urls[index].short_url)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>URL Shortener</h1>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input type="text" name="url" id="url" placeholder="Enter URL"
+          className="urlInput"
+          onChange={e => setLongUrl(e.target.value)} />
+        <button type="submit"
+          className="submitBtn"
+        >Shorten</button>
+      </form>
+      <div>
+        {urls.map((url, index) => (
+          <div key={index} onClick={e => handleClick(index)}
+            className="urlListItem">
+            <a className="fullUrl">
+              {url.original_url}</a>
+            <a className="shortUrl">
+              {url.short_url}</a>
+          </div>
+        ))}
+      </div>
     </div>
+
   );
 }
+
+
 
 export default App;
